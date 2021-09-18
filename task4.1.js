e6c5ff745755ebd61213970857f6434ffd5fc91e
madis <madis.pihlasalu@gmail.com>
2021-09-18 22:34:57 +0300

    array join function

---
+++ task4.1.js
@@ -0,0 +1,8 @@
+const colors = ["Red", "Green", "White", "Black"]
+for (let i =0; i < 1; i++){
+    if (i===1)
+    break
+
+console.log('"'+(colors.join (",")+'"'))
+console.log('"'+colors.join (" , ")+'"')
+console.log('"'+colors.join (" + ")+'"')}
